9ac83697a9bbd5ac8de119d81aab3abc6a93e886
SincerityHun <120380703+SincerityHun@users.noreply.github.com>
2023-04-07 22:54:06 +0900

    wordle word listview part complete

--- PA1/app/src/main/java/edu/skku/cs/pa1/MainActivity.kt
+++ PA1/app/src/main/java/edu/skku/cs/pa1/MainActivity.kt
@@ -2,24 +2,60 @@ package edu.skku.cs.pa1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ListView
-import android.widget.TextView
+import android.text.Editable
+import android.widget.*
+import java.io.File
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val wordleText = findViewById<EditText>(R.id.wordleText)
-        val submitBtn = findViewById<Button>(R.id.submitBtn)
-        val items = ArrayList<wordList>()
+        val wordleText = findViewById<EditText>(R.id.wordleText) //1. wordle edit text part
+        val submitBtn = findViewById<Button>(R.id.submitBtn) //2. wordle text submit button part
+        val items = ArrayList<wordList>() //3. 사용자가 예측한 단어를 저장해 놓는 배열
 
+        //4. file 받아와서 answer word 랜덤하게 뽑기
+        val fileInputStream = applicationContext.assets.open("wordle_words.txt")
+        val fileInputString = fileInputStream.readBytes().toString(Charsets.UTF_8)
+        val fileInputList = fileInputString.split("\n").map{it.trim()} //4-1. 전체 dictionary word list
+        var random = Random()
+        var randomNum = random.nextInt(fileInputList.count())
+        val answer = fileInputList.get(randomNum) //4-2. answer text 임의로 선정하기
+        println("total length: ${fileInputList.count()}, randomNum: ${randomNum}, random word: ${answer}") //TEST
+
+        //5. submit button
         submitBtn.setOnClickListener {
-            items.add(wordList(wordleText.toString().toList(), listOf(1,1,1,1,1)))
-            val wordListView = findViewById<ListView>(R.id.wordListView)
-            val myAdapter = wordListAdapter(applicationContext,items)
-            wordListView.adapter = myAdapter
+            val inputWord = wordleText.text.toString().toLowerCase().trim() // 5-1. input word를 소문자화 하고 trim 완료
+            if(fileInputList.contains(inputWord)) //5-2. input word가 사전에 있는 경우
+            {
+                val integerList = wordle(inputWord,answer) // 정답 int list
+                items.add(wordList(inputWord.toUpperCase().toList(),integerList))
+                // 5-2. word list 구현 완료
+                val wordListView = findViewById<ListView>(R.id.wordListView)
+                val myAdapter = wordListAdapter(applicationContext,items)
+                wordListView.adapter = myAdapter
+                wordleText.text = Editable.Factory.getInstance().newEditable("")
+            }
+            else{ //5-2. input word가 사전에 없는 경우
+                Toast.makeText(applicationContext,"Word '${inputWord}' not in dictionary!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
+    fun wordle(word1: String, word2: String): List<Int> { //wordle 구분 함수
+        val result = mutableListOf<Int>()
+        for (i in 0 until 5) {
+            if (word1[i] == word2[i]) {
+                result.add(2)
+            } else if (word2.contains(word1[i])) {
+                result.add(1)
+            } else {
+                result.add(0)
+            }
+        }
+        return result
+    }
+
+
 }
--- PA1/app/src/main/java/edu/skku/cs/pa1/wordListAdapter.kt
+++ PA1/app/src/main/java/edu/skku/cs/pa1/wordListAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 class wordListAdapter(val context:Context, val data: ArrayList<wordList>): BaseAdapter()  {
     override fun getCount(): Int {
@@ -29,13 +30,30 @@ class wordListAdapter(val context:Context, val data: ArrayList<wordList>): BaseA
         val text3 = generatedView.findViewById<TextView>(R.id.thirdText)
         val text4 = generatedView.findViewById<TextView>(R.id.fourText)
         val text5 = generatedView.findViewById<TextView>(R.id.fiveText)
-
+        //1. Text 반영
         text1.text = data[p0].word.get(0).toString()
         text2.text = data[p0].word.get(1).toString()
         text3.text = data[p0].word.get(2).toString()
         text4.text = data[p0].word.get(3).toString()
         text5.text = data[p0].word.get(4).toString()
+
+        //2. 색깔 변경
+        colorCheck(text1,data[p0].color.get(0))
+        colorCheck(text2,data[p0].color.get(1))
+        colorCheck(text3,data[p0].color.get(2))
+        colorCheck(text4,data[p0].color.get(3))
+        colorCheck(text5,data[p0].color.get(4))
         return generatedView
+    }
+    fun colorCheck(p0:TextView,p1:Int){  //객체의 배경색과 글자색을 바꿔주는 함수
+        when(p1){
+            0 -> {  p0.setBackgroundColor(ContextCompat.getColor(context,R.color.background_out))
+            p0.setTextColor(ContextCompat.getColor(context,R.color.text_out)) }
+            1 -> {  p0.setBackgroundColor(ContextCompat.getColor(context,R.color.background_ball))
+                p0.setTextColor(ContextCompat.getColor(context,R.color.text_ball)) }
+            2 -> {  p0.setBackgroundColor(ContextCompat.getColor(context,R.color.background_strike))
+                p0.setTextColor(ContextCompat.getColor(context,R.color.text_strike )) }
+        }
     }
 
 }
